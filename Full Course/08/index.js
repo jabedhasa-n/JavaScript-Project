5465b535eb08ba6a0830e71b006d008db579c546
jabed366 <jabedhasan231@gmail.com>
2021-11-05 15:45:05 +0600

    First Commit

--- Full Course/08/index.js
+++ Full Course/08/index.js
@@ -1,14 +1,35 @@
+//character function
+
 var country="Bangladesh";
+//text lengh
 var len=country.length;
 console.log(len);
 document.write(len);
-
 document.write("<br>");
 
-var firstName=prompt("Your First Name");
+//excet character 
+document.write(country.charAt(3));
+document.write("<br>");
+ 
+//uppercase 
+document.write(country.toUpperCase());
+document.write("<br>");
 
+//lowecase 
+document.write(country.toLowerCase);
+document.write("<br>");
+
+//input charcter
+var firstName=prompt("Your First Name");
 document.write(firstName);
 document.write("<br>");
 var len=firstName.length;
-
 document.write(len);
+document.write("<br>");
+
+//concatanation
+var country="Bangladesh ";
+var text="is my country";
+var stringAdd=(country.concat(text));
+document.write(stringAdd);
+
