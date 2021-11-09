4d6122a644f1b8669f0bc750b8793d57eb944b43
jabed366 <jabedhasan231@gmail.com>
2021-11-09 21:32:39 +0600

    First Commit

--- 1/index1.js
+++ 1/index1.js
@@ -1,16 +1,16 @@
-// var firstName = prompt("Please Type your First Name");
-// var lastName= prompt(" Please Type your Last Name");
-// var sscYear = prompt("Please Type your SSC year");
-// var bestDevtools = prompt("Please Type your best dev tool (ex:Js,c,java etc)");
-// var bestDevExperience = prompt("how many years do you have expeience this tools?");
-// var newDevtools = prompt("Please Tell Us your new dev tools");
+var firstName = prompt("Please Type your First Name");
+var lastName= prompt(" Please Type your Last Name");
+var sscYear = prompt("Please Type your SSC year");
+var bestDevtools = prompt("Please Type your best dev tool (ex:Js,c,java etc)");
+var bestDevExperience = prompt("how many years do you have expeience this tools?");
+var newDevtools = prompt("Please Tell Us your new dev tools");
 
 
-// console.log( "My Name is"+ firstName + lastName);
-// console.log( "My SSC year is "+ sscYear);
-// console.log( "I am expert in "+ bestDevtools);
-// console.log( "My experince of " + bestDevtools +"is " + bestDevExperience +" years");
-// console.log( "But I am learning new technology that is "+ newDevtools);
+console.log( "My Name is"+ firstName + lastName);
+console.log( "My SSC year is "+ sscYear);
+console.log( "I am expert in "+ bestDevtools);
+console.log( "My experince of " + bestDevtools +"is " + bestDevExperience +" years");
+console.log( "But I am learning new technology that is "+ newDevtools);
 
 
 
--- 1/index3.js
+++ 1/index3.js
@@ -1,12 +1,42 @@
 var groomName = prompt("Type the groom's Name");
-var witnessName =prompt("who want to get a witness");
-var witnessConfirm =confirm("Does " + witness + "want to get a witness")
+var gwitnessName =prompt("who want to be a witness");
+var gwitnessConfirm =confirm("Does " + gwitnessName + " wantS to get a witness");
 
-var groomConfirm =confirm( groomName+ "please tell kobul")
-var witnessConfirm =confirm( groomName+ "please tell kobul")
+var groomConfirm =confirm( groomName+ " please tell kobul");
+var sayAlhamdulillah =confirm(" everone say alhamdulillah?");
 
 
 
 var brideName = prompt("Type the bride's Name");
-var witnessName =prompt("who want to get a witness");
-var witnessConfirm =confirm("Does " + witness + "want to get a witness")
+var pwitnessName =prompt("who want to get a witness");
+var pwitnessConfirm =confirm("Does " + pwitnessName + "want to get a witness")
+
+var brideConfirm =confirm( brideName+ " please tell kobul");
+var sayAlhamdulillah =confirm(" everone say alhamdulillah?");
+
+var fullConfirmKobul =confirm(" Married are confirm.right?");
+///console part start---
+
+
+//groom part
+console.log( "the groom's Name is "+ groomName);
+console.log( gwitnessName +" is the witness ");
+console.log( "is it done by witness ? " + gwitnessConfirm);
+
+
+console.log( "is it done by groom ? " + groomConfirm);
+console.log( " everyone say alhmdlilldh? " + sayAlhamdulillah);
+
+//pride part 
+console.log( "the groom's Name is "+ brideName);
+console.log( pwitnessName +" is the witness ");
+console.log( "is it done by witness ? " + pwitnessConfirm);
+
+
+console.log( "is it done by groom ? " + proomConfirm);
+console.log( " everyone say alhmdlilldh? " + sayAlhamdulillah);
+
+
+console.log( " married are confrim" + fullConfirmKobul);
+
+
