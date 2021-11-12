f5d2ee83afcc2ea6eedc95763dc07a033439448d
jabed366 <jabedhasan231@gmail.com>
2021-11-12 21:09:15 +0600

    First Commit

--- 2nd Day/index3.js
+++ 2nd Day/index3.js
@@ -1,70 +1,60 @@
-let yourGender =prompt("Please type Your Gender(Male/Female");
+// let yourGender =prompt("Please type Your Gender(Male/Female");
 
-if(yourGender="Male"){
-    let boyAge =prompt("Hi Brother,Please type your age");
-    let girlAge =prompt("Hi Brother,Please type your Fiance's age");
-}
+// let boyAge;
+// let girlAge;
 
-else if(yourGender="male"){
-    let boyAge =prompt("Hi Brother,Please type your age");
-    let girlAge =prompt("Hi Brother,Please type your Fiance's age");
-
-    if (boyAge>=21 && girlAge>=18){
-      console.log("Boys age is" + boyAge);
-      console.log("girl age is" + girlAge);
+// if(yourGender=="Male" || yourGender=="male"){
+//     boyAge =prompt("Hi Brother,Please type your age");
+//     girlAge =prompt("Hi Brother,Please type your Fiance's age");
+//    if (boyAge>=21 && girlAge>=18){
+//       console.log("Boys age is" + boyAge);
+//       console.log("girl age is" + girlAge);
   
-     console.log("So , They are capable to marry");
-  }
-  else if(boyAge>=21 && girlAge<=18){
-      console.log("So , They are are not capable to marry");
-      console.log("please boy, you don't need to wait but you finance ....");
-      console.log("please girl, you have to wait " +(18-girlAge) + "years");
-   }
-  else if(boyAge<=21 && girlAge>=18){
-      console.log("So , They are are not capable to marry");
-      console.log("please boy, you have to wait " +(21-boyAge) + "years");
-      console.log("please girl, you don't need to wait but you finance ....");
-   }
-  else{
-      console.log("So , They are are not capable to marry");
-      console.log("please boy, you have to wait " +(21-boyAge) + "years");
-      console.log("please girl, you have to wait " +(18-girlAge) + "years");
-   };
-}
-else if(yourGender="Female"){
-    let girlAge =prompt("Hi Sister,Please type your age");
-    let boyAge =prompt("Hi Sister,Please type your Fiance's age");
-}
-else if(yourGender="female"){
-    let girlAge =prompt("Hi Sister,Please type your age");
-    let boyAge =prompt("Hi Sister,Please type your Fiance's age");
-}
+//      console.log("So , They are capable to marry");
+//   }
+//   else if(boyAge>=21 && girlAge<=18){
+//       console.log("So , They are are not capable to marry");
+//       console.log("please boy, you don't need to wait but you finance ....");
+//       console.log("please girl, you have to wait " +(18-girlAge) + "years");
+//    }
+//   else if(boyAge<=21 && girlAge>=18){
+//       console.log("So , They are are not capable to marry");
+//       console.log("please boy, you have to wait " +(21-boyAge) + "years");
+//       console.log("please girl, you don't need to wait but you finance ....");
+//    }
+//   else{
+//       console.log("So , They are are not capable to marry");
+//       console.log("please boy, you have to wait " +(21-boyAge) + "years");
+//       console.log("please girl, you have to wait " +(18-girlAge) + "years");
+//    };
+// }
 
-else{
-  console.log("wrong,please type again")
-}
-
-
-if (boyAge>=21 && girlAge>=18){
-   console.log("Boys age is" + boyAge);
-   console.log("girl age is" + girlAge);
-
-  console.log("So , They are capable to marry");
-}
-else if(boyAge>=21 && girlAge<=18){
-   console.log("So , They are are not capable to marry");
-   console.log("please boy, you don't need to wait but you finance ....");
-   console.log("please girl, you have to wait " +(18-girlAge) + "years");
-}
-else if(boyAge<=21 && girlAge>=18){
-   console.log("So , They are are not capable to marry");
-   console.log("please boy, you have to wait " +(21-boyAge) + "years");
-   console.log("please girl, you don't need to wait but you finance ....");
-}
-else{
-   console.log("So , They are are not capable to marry");
-   console.log("please boy, you have to wait " +(21-boyAge) + "years");
-   console.log("please girl, you have to wait " +(18-girlAge) + "years");
-};
-
+// else if(yourGender=="Female" || yourGender=="female"){
+//      girlAge =prompt("Hi Sister,Please type your age");
+//      boyAge =prompt("Hi Sister,Please type your Fiance's age");
+//      if (boyAge>=21 && girlAge>=18){
+//       console.log("Boys age is" + boyAge);
+//       console.log("girl age is" + girlAge);
+  
+//      console.log("So , They are capable to marry");
+//   }
+//   else if(boyAge>=21 && girlAge<=18){
+//       console.log("So , They are are not capable to marry");
+//       console.log("please boy, you don't need to wait but you finance ....");
+//       console.log("please girl, you have to wait " +(18-girlAge) + "years");
+//    }
+//   else if(boyAge<=21 && girlAge>=18){
+//       console.log("So , They are are not capable to marry");
+//       console.log("please boy, you have to wait " +(21-boyAge) + "years");
+//       console.log("please girl, you don't need to wait but you finance ....");
+//    }
+//   else{
+//       console.log("So , They are are not capable to marry");
+//       console.log("please boy, you have to wait " +(21-boyAge) + "years");
+//       console.log("please girl, you have to wait " +(18-girlAge) + "years");
+//    };
+// }
+// else{
+//   console.log("wrong,please type again")
+// };
 
--- 2nd Day/practice.js
+++ 2nd Day/practice.js
@@ -3,23 +3,23 @@ let boyAge =prompt("Please type boy's age");
 let girlAge =prompt("Please type girl's age");
 
 if (boyAge>=21 && girlAge>=18){
-    console.log("Boys age is" + boyAge);
-    console.log("girl age is" + girlAge);
+    console.log("Boys age is " + boyAge);
+    console.log("girl age is " + girlAge);
 
    console.log("So , They are capable to marry");
 }
 else if(boyAge>=21 && girlAge<=18){
     console.log("So , They are are not capable to marry");
-    console.log("please boy, you don't need to wait but you finance ....");
-    console.log("please girl, you have to wait " +(18-girlAge) + "years");
+    console.log("please boy, you don't need to wait but you finance .....So find out another girl");
+    console.log("please girl, you have to wait " +(18-girlAge) + " years");
  }
 else if(boyAge<=21 && girlAge>=18){
     console.log("So , They are are not capable to marry");
-    console.log("please boy, you have to wait " +(21-boyAge) + "years");
-    console.log("please girl, you don't need to wait but you finance ....");
+    console.log("please boy, you have to wait " +(21-boyAge) + " years");
+    console.log("please girl, you don't need to wait but you finance ....So find out another boy");
  }
 else{
     console.log("So , They are are not capable to marry");
-    console.log("please boy, you have to wait " +(21-boyAge) + "years");
-    console.log("please girl, you have to wait " +(18-girlAge) + "years");
+    console.log("please boy, you have to wait " +(21-boyAge) + " years");
+    console.log("please girl, you have to wait " +(18-girlAge) + " years");
  };
