8c156002104f69d01be275349bc191d16cbe1bba
jabed366 <jabedhasan231@gmail.com>
2021-11-14 23:53:17 +0600

    First Commit

--- 2nd Day/index1.js
+++ 2nd Day/index1.js
@@ -12,31 +12,32 @@ let chemistry=parseInt
 let final= (english + bangla + mathematics + physics + chemistry)/5;
 
 
-if(final>=0 && final <=32){
+if(final>=0 && final <=32 ){
 
   console.log( "Your average marks is " + final + " and Your grade is F");
 
 }
-else if(final>=33 && final <=39){
+else if(final>=33 && final <=39 && english>=33 && chemistry>=33 && physics>=33 && mathematics>=33 && bangla>=33){
     console.log( "Your average marks is " + final + " and Your grade is C");
+    
 }
-else if(final>=40 && final <=49){
+else if(final>=40 && final <=49 && english>=33 && chemistry>=33 && physics>=33 && mathematics>=33 && bangla>=33){
     console.log( "Your average marks is " + final + " and Your grade is D");
 }
-else if(final>=50 && final <=59){
+else if(final>=50 && final <=59 && english>=33 && chemistry>=33 && physics>=33 && mathematics>=33 && bangla>=33){
     console.log( "Your average marks is " + final + " and Your grade is B");
 }
-else if(final>=60 && final <=69){
+else if(final>=60 && final <=69 && english>=33 && chemistry>=33 && physics>=33 && mathematics>=33 && bangla>=33){
     console.log( "Your average marks is " + final + " and Your grade is A-");
 }
-else if(final>=70 && final <=79){
+else if(final>=70 && final <=79 && english>=33 && chemistry>=33 && physics>=33 && mathematics>=33 && bangla>=33){
     console.log( "Your average marks is " + final + " and Your grade is A");
 }
-else if(final>=80 && final <=100){
+else if(final>=80 && final <=100 && english>=33 && chemistry>=33 && physics>=33 && mathematics>=33 && bangla>=33){
     console.log( "Your average marks is " + final + " and Your grade is A+");
 }
 else {
-    console.log("It is undefined");
+    console.log("Your average marks is " + final +  " he is F");
 };
 
 
