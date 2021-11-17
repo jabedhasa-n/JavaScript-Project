16498cb6de29f3ce3fe1a0dbff99f239b6612f0f
jabed366 <jabedhasan231@gmail.com>
2021-11-18 02:17:20 +0600

    First Commit

--- 4th Day/index6.js
+++ 4th Day/index6.js
@@ -1,8 +1,6 @@
 // Create a loop where the initial value is 10,000. You need to rotate the loop 50 times by using the increment and decrement operator twice.
-
-for(let i=10000;i>=9975;i--){
+let j;
+for(let i=10000,j=0; i>=9975;i--,j++){
     console.log(i);
-    for(let n=10000;n<=10025;n++){
-        console.log(n);
-    }
+    console.log(j);
 }
