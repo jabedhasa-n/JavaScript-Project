28269b23d526177c89884cad939e17d07107ff65
jabed366 <jabedhasan231@gmail.com>
2021-11-19 03:30:55 +0600

    First Commit

--- 4th Day/practice.js
+++ 4th Day/practice.js
@@ -1,25 +1,35 @@
-// let age=10;
 
-// //ternery operator 
+//ternery operator 
+let age=10;
+let hello=(age>=18)? "right": "wrong";
+console.log(hello);
 
-// let hello=(age>=18)? "right": "wrong";
+// nallesh collasing operator
+let ope=null;
+console.log(ope ?? 'ok');
 
-// console.log(hello)
-
-
-// // nallesh collasing operator
-
-// let ope=null;
-
-// console.log(ope ?? 'ok')
-
-// let name = "Jabed";
-
-// let right=(name=="Jabed") ? 'he is a right' : 'he is not right'
-
-// console.log(right)
+//ternery operator 
+let name = "Jabed";
+let right=(name=="Jabed") ? 'he is a right' : 'he is not right';
+console.log(right)
 
 
-// for(let i=0; i<100;i++){
-//     console.log(i);
-// }
+///for loop
+for(let i=1;i<=10;i++){
+    console.log(i);
+}
+
+//while loop
+let n=1;
+while(n<=10){
+    console.log(n);
+    n++;
+}
+
+//do-while lopp
+let d=1;
+do{
+    console.log(d);
+    d++;
+}while(d<=10);
+
