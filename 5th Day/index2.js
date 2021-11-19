b5de06f2ee4a94a8e709468903325970c6e0b471
jabed366 <jabedhasan231@gmail.com>
2021-11-19 22:27:24 +0600

    First Commit

--- 5th Day/index2.js
+++ 5th Day/index2.js
@@ -1,21 +1,21 @@
-// // Create a function that will return children, teenagers, young people, old people from the age
+// Create a function that will return children, teenagers, young people, old people from the age
 
-// function ageVariation (a){
+function ageVariation (a){
 
-//     if( a>=0 && a<=7){
-//         console.log("You are Children")
-//     }
-//     else if( a>=8 && a<=19){
-//         console.log("You are a Teenager")
-//     }
-//     else if( a>=20 && a<=35){
-//         console.log("You are a Young people")
-//     }
-//     else {
-//         console.log("You are a old")
-//     }
+    if( a>=0 && a<=7){
+        console.log("You are Children")
+    }
+    else if( a>=8 && a<=19){
+        console.log("You are a Teenager")
+    }
+    else if( a>=20 && a<=35){
+        console.log("You are a Young people")
+    }
+    else {
+        console.log("You are a old")
+    }
   
-//   }
-//   let age=parseInt(prompt("please Type your age"));
+  }
+  let age=parseInt(prompt("please Type your age"));
   
-//   console.log(ageVariation(age));
+  console.log(ageVariation(age));
