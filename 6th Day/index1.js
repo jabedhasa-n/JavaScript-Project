6e3c5b91beabf5c81bcd694262bdac36abf0ef43
jabed366 <jabedhasan231@gmail.com>
2021-11-20 21:52:34 +0600

    First Commit

--- 6th Day/index1.js
+++ 6th Day/index1.js
@@ -1,20 +1,26 @@
 // GPA, CGPA, GRADE function for result publishing
 
-let studentName = prompt("Please Type Your Name")
-let studentroll = prompt("Please Type Your Roll")
-let bangla = parseInt(prompt("please Type Your Bangla Mark"))
-let english = parseInt(prompt("please Type Your English Mark"))
+let studentName = prompt("Please Type Your Name");
+let studentroll = prompt("Please Type Your Roll");
+let bangla = parseInt(prompt("please Type Your Bangla Mark"));
+let english = parseInt(prompt("please Type Your English Mark"));
 let math = parseInt(prompt("please Type Your Math Mark"));
 
 
 
+
+
 console.log(
-    `stundet name = ${studentName}
-     stundet roll = ${studentroll}
+    `
+    stundet name = ${studentName}
+    stundet roll = ${studentroll}
      
     subject        marks             GPA                grade
     bangla         ${bangla}         ${gpa(bangla)}     ${grade(bangla)}
-    English        ${english}        ${gpa(english)}    ${grade(bangla)}
-    Mathematics    ${math}           ${gpa(math)}       ${grade(bangla)}
+    English        ${english}        ${gpa(english)}    ${grade(english)}
+    Mathematics    ${math}           ${gpa(math)}       ${grade(math)}
+
+    ------------------------------------------------------------------------------
+                                            cgpa= ${cgpa(gpa(bangla),gpa(english),gpa(math))} ;
     `
 )
