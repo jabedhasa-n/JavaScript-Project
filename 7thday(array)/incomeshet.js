d75844e0379959ee0cb228c3627cc0b9eae781c1
jabed366 <jabedhasan231@gmail.com>
2021-11-28 23:52:40 +0600

    First Commit

--- 7thday(array)/incomeshet.js
+++ 7thday(array)/incomeshet.js
@@ -1,1 +1,23 @@
 // - Create a MERN stack, Laravel, Django, GoLang,  WordPress, java dives students data structure and make their total income sheet with per developer data, Find specific students.
+
+
+const devTools=[
+   ['asif',30,'Laravel','Australia',30000],
+   ['akbar',30,'Django','Bangladesh',40000],
+   ['jahir',30,'GoLang','India',50000],
+   ['abul',30,'Java','Australia',60000],
+   ['kamal',30,'Django','Bangladesh',70000],
+   ['mojib',30,'GoLang','India',80000],
+   ['tanha',30,'WordPress','Australia',90000],
+   ['afrin',30,'Laravel','Bangladesh',130000],
+   ['jamal',30,'WordPress','Australia',140000],
+   ['mofiz',30,'Java','India',150000]
+];
+
+
+for(let i =0; i<devTools.length;i++){
+
+    console.log(devTools[i]);
+
+    // if(devTools[i])
+}
