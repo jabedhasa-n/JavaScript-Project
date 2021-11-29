03ecd9e8ac63291dc5336c8961b20952a13bd02e
jabed366 <jabedhasan231@gmail.com>
2021-11-29 19:58:36 +0600

    First Commit

--- 7thday(array)/ageVariation.js
+++ 7thday(array)/ageVariation.js
@@ -1,24 +1,17 @@
 // - Send a welcome message to everyone over the age of 18 based on the age of the data array of all the members of a club and send a greeting message to the person over the age of 40, of course, send the message through a function, and to those who are under 18 years of age. Give the message to be attentive.'
 
 
-
-
-
-
-// - Create a MERN stack, Laravel, Django, GoLang,  WordPress, java dives students data structure and make their total income sheet with per developer data, Find specific students.
-
-
 const ageVariation=[
-    ['asif',18,'Laravel','Australia',30000],
-    ['akbar',33,'Django','Bangladesh',40000],
-    ['jahir',30,'GoLang','India',50000],
-    ['abul',20,'Java','Australia',60000],
-    ['kamal',30,'Django','Bangladesh',70000],
-    ['mojib',40,'GoLang','India',80000],
-    ['tanha',50,'WordPress','Australia',90000],
-    ['afrin',13,'Laravel','Bangladesh',130000],
-    ['jamal',70,'WordPress','Australia',140000],
-    ['mofiz',30,'Java','India',150000]
+    ['asif',18,'Laravel','Australia'],
+    ['akbar',33,'Django','Bangladesh'],
+    ['jahir',30,'GoLang','India'],
+    ['abul',20,'Java','Australia'],
+    ['kamal',30,'Django','Bangladesh'],
+    ['mojib',40,'GoLang','India'],
+    ['tanha',50,'WordPress','Australia'],
+    ['afrin',13,'Laravel','Bangladesh'],
+    ['jamal',70,'WordPress','Australia'],
+    ['mofiz',30,'Java','India']
  ];
  
  
