92526c57b541d079c457058bf9a44be341fac3ba
jabed366 <jabedhasan231@gmail.com>
2021-12-09 22:30:38 +0600

    First Commit

---
+++ 9thdayMath&Date/result.js
@@ -0,0 +1,34 @@
+//string to number
+
+let numbe="1200";
+
+console.log(typeof Number(numbe));
+console.log(typeof parseInt(numbe));
+console.log(typeof parseFloat(numbe));
+console.log(typeof +numbe);
+
+
+console.log("================")
+
+// number to string
+ number=1200;
+
+console.log(typeof number.toString());
+console.log(typeof number.toExponential())
+console.log(typeof number.toPrecision())
+console.log(typeof toString(number));
+console.log(typeof String(number));
+
+
+//bolean to number 
+console.log("================")
+let go=true;
+
+console.log(typeof String(go));
+
+
+//boolean to string
+console.log("================")
+go=false;
+
+console.log(typeof Number(go));
