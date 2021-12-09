2a9b535ffc6663f1eae9caaa8e9160fcc3eef3a5
jabed366 <jabedhasan231@gmail.com>
2021-12-09 22:07:20 +0600

    First Commit

---
+++ 9thdayMath&Date/mathproperty.js
@@ -0,0 +1,14 @@
+
+let ammount =20.99;
+
+console.log(Math.round(ammount));
+console.log(Math.abs(ammount));
+console.log(Math.ceil(ammount));
+console.log(Math.floor(ammount))
+
+console.log(Math.min(6,4,1,5))
+console.log(Math.max(6,4,1,5))
+
+console.log(Math.pow(2,3))
+
+console.log(Math.ceil(Math.random() * 10000))
