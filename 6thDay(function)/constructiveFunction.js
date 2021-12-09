0ebb100de09bf8dad0c5f295bab0a2b3eb542196
jabed366 <jabedhasan231@gmail.com>
2021-12-09 19:54:20 +0600

    First Commit

---
+++ 6thDay(function)/constructiveFunction.js
@@ -0,0 +1,18 @@
+// hing
+// Create an age calculator function
+
+
+let MainFunction=function(){
+
+
+    this.birthYear=(year)=>{
+        return `Your age is now ${2021-year} years old`;
+    }
+}
+
+let year=parseInt(prompt("please Type your Birth Year"));
+
+let lead_Function=new MainFunction();
+
+console.log(lead_Function.birthYear(year));
+
--- 8th dayObject array/result.js
+++ 8th dayObject array/result.js
@@ -1,16 +1,44 @@
-let todayDate = new Date();
-
-console.log(todayDate);
-console.log(todayDate.getDate());
-console.log(todayDate.getMonth())
-console.log(todayDate.getFullYear())
+bardate=['sun','mon','tue','wed','thu','fri','sat']
 
 
-console.log (`
-        
-              Date  ${todayDate.getDate()>9 ? todayDate.getDate() : '0' + todayDate.getDate()} 
-              Month ${todayDate.getMonth()+1}
-              Year :${todayDate.getFullYear()}
-              time : ${todayDate.getHours()>12 ? todayDate.getHours()-12 : todayDate.getHours()} ${todayDate.getMinutes()} ${todayDate.getSeconds()} ${todayDate.getHours()>12 ? 'PM' : 'AM'}
+let todaydate = new Date();
+
+console.log(`
+          
+         ${bardate[todaydate.getDay()]}
+
 `)
 
+
+
+//swith statement
+let day ;
+switch (todaydate.getDay()){
+  case 3:
+    day="thursday";
+    break;
+  case 4:
+   day= "thursday";
+    break;
+  case 5:
+    day="thursday";
+    break;
+  case 6:
+    day="thursday";
+    break;
+
+  default:
+    break;
+}
+
+console.log(day)
+
+//function day 
+
+myfunction =(date)=>{
+  bardate=['sun','mon','tue','wed','thu','fri','sat']
+  return bardate[date]
+}
+
+todaydate = new Date();
+console.log(myfunction(todaydate.getDay()));
