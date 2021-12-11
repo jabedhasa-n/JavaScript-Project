00c82c8430b431725de18b8cf525c8c4c288bb72
jabed366 <jabedhasan231@gmail.com>
2021-12-11 18:08:02 +0600

    First Commit

--- 9thdayMath&Date/result.js
+++ 9thdayMath&Date/result.js
@@ -1,34 +1,20 @@
-//string to number
 
-let numbe="1200";
+function mulcal (a,b,c){
+	if (a =='t'){
+    	 return  0.5*b*c;
+    }
+     else if (a == 's' ){
+    	return b*c;
+    }
+    else if (a == 'r'){
+    	return b*c;
+    }
 
-console.log(typeof Number(numbe));
-console.log(typeof parseInt(numbe));
-console.log(typeof parseFloat(numbe));
-console.log(typeof +numbe);
+}
 
-
-console.log("================")
-
-// number to string
- number=1200;
-
-console.log(typeof number.toString());
-console.log(typeof number.toExponential())
-console.log(typeof number.toPrecision())
-console.log(typeof toString(number));
-console.log(typeof String(number));
+let abc = prompt("What you want to find:");
+let lenth = parseInt(prompt("Enter your lenth:"));	
+let height = parseInt(prompt("Enter your height:"));
 
 
-//bolean to number 
-console.log("================")
-let go=true;
-
-console.log(typeof String(go));
-
-
-//boolean to string
-console.log("================")
-go=false;
-
-console.log(typeof Number(go));
+console.log(mulcal(abc,lenth,height));
