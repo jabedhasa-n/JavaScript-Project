f60483704e725cad50ca85ca4ad34ad0c697e75e
jabed366 <jabedhasan231@gmail.com>
2021-12-12 02:15:57 +0600

    First Commit

---
+++ 7thday(array)/sliceAndSplice.js
@@ -0,0 +1,33 @@
+
+
+let ages = [32, 33, 16, 40];
+
+
+let ageagain=ages.splice(1,2)
+// prothom 1 ta bad ,porer 2 ta thkhabe (cut) hoye jabe and ageagain a man asbe [33][16]
+
+// let ageagain=ages.splice(2,2)
+//prothom 2 ta bad ,porer 2 ta thkhabe (cut)
+// let ageagain=ages.splice(1,2)
+//prothom 1 ta bad ,porer 2 ta thkhabe (cut) + 
+
+
+console.log(ages)
+console.log(ageagain)
+
+
+console.log("=======================")
+
+ ages = [32, 33, 16, 40];
+
+ ageagain=ages.slice(1,2)
+
+console.log(ages)
+
+
+console.log(ageagain)
+//result will be [33]
+//prothom ta 1 ta bad porer ta bosbe .mane 33 bosbe ( copy hobe ageagain a a)
+
+
+
