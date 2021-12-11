8dc443faf73666b3168d381dd3308ad8c0dd3aca
jabed366 <jabedhasan231@gmail.com>
2021-12-11 21:52:20 +0600

    First Commit

---
+++ 10thdayJSON/practice.js
@@ -0,0 +1,9 @@
+
+
+localStorage.setItem('username', 'Jabed');
+localStorage.setItem('password','124');
+console.log(localStorage);
+console.log(localStorage.getItem('username'));
+console.log(localStorage.removeItem('username'));
+localStorage.removeItem('password')
+localStorage.clear()
