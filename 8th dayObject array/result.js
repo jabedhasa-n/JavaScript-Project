cd323727873abdde1bfc1f61c4824450e3581054
jabed366 <jabedhasan231@gmail.com>
2021-12-14 18:26:43 +0600

    First Commit

--- 8th dayObject array/result.js
+++ 8th dayObject array/result.js
@@ -19,68 +19,69 @@
 
 
 let student =[
-    {
-       name : "Jabed",
-       fatherName:"Mohib Ullah",
-       bangla : 40,
-       english : 50,
-       physics : 60,
-       chemistry : 70,
-       math : 80,
-    },
-    {
-       name : "saki",
-       fatherName:"Mohib Ullah",
-       bangla : 40,
-       english : 50,
-       physics : 60,
-       chemistry : 70,
-       math : 80,
-    },
-    {
-       name : "imran",
-       fatherName:"Mohib Ullah",
-       bangla : 40,
-       english : 60,
-       physics : 60,
-       chemistry : 70,
-       math : 80,
-    },
+   {
+      name : "Jabed",
+      fatherName:"Mohib Ullah",
+      bangla : 40,
+      english : 50,
+      physics : 60,
+      chemistry : 70,
+      math : 80,
+   },
+   {
+      name : "saki",
+      fatherName:"Mohib Ullah",
+      bangla : 40,
+      english : 50,
+      physics : 60,
+      chemistry : 70,
+      math : 80,
+   },
+   {
+      name : "imran",
+      fatherName:"Mohib Ullah",
+      bangla : 40,
+      english : 60,
+      physics : 60,
+      chemistry : 70,
+      math : 80,
+   },
 ]
 
 
 let MainFunction = function(){
-   this.result=function(a,b,c,d,e){
-       if(a<32 || b<32 || c<32 || d<32 || e<32){
-           return "You are faild";
-       }
-       else {
-           let sum=0;
-           sum=(a+b+c+d+e)/6;
-           return `you are passed .your cgpa result is ${sum}`
-       }
-   }
+  this.result=function(a,b,c,d,e){
+      if(a<32 || b<32 || c<32 || d<32 || e<32){
+          return "You are faild";
+      }
+      else {
+          let sum=0;
+          sum=(a+b+c+d+e)/6;
+          return `you are passed .your cgpa result is ${sum}`
+      }
+  }
 
 }
 
 let lead_function = new MainFunction;
 
 student.forEach((data)=>{
-   console.log(data)
-   console.log(data.bangla)
-   
+  console.log(data)
+  console.log(data.bangla)
+  
 })
 
 
 for(let i=0;i<student.length;i++){
-   console.log(
-       `
-       ${student[i].name}
-       ${student[i].fatherName}
-       ======================
-       gpa=${lead_function.result(student[i].bangla,student[i].english,student[i].physics,student[i].chemistry,student[i].math)};
-       `
-    )
-   
+  console.log(
+      `
+      ${student[i].name}
+      ${student[i].fatherName}
+      ======================
+      gpa=${lead_function.result(student[i].bangla,student[i].english,student[i].physics,student[i].chemistry,student[i].math)};
+      `
+   )
+  
 }
+console.log(JSON.stringify(student))
 
