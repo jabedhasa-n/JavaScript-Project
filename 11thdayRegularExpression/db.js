5705ab592ce482bea4624f52be3e45406e6fc04b
jabed366 <jabedhasan231@gmail.com>
2021-12-21 18:56:33 +0600

    First Commit

--- 11thdayRegularExpression/db.js
+++ 11thdayRegularExpression/db.js
@@ -1,15 +1,34 @@
-let bdNumber="+8801793534981";
-
-let pattern=/^(01|\+8801)[0-9]{9}$/
-console.log(pattern.test(bdNumber))
 
 
+// # Create an email pattern 
+
+let userEmil="sakivai231@gmail.com";
+
+let pattern=/^[a-z0-9_\.-]*@[a-z0-9\-]*\.[a-z]{2,5}$/;
+
+console.log(pattern.test(userEmil));
+
+// # Create a Bangladeshi phone number pattern
+
+let phoneNumber="01793534981";
+pattern=/^(01|\+08801)[0-9]{9}$/;
+console.log(pattern.test(phoneNumber));
 
 
-let myEmail="jabedhasan231@gmail.com";
+// # Create a password pattern 
 
-pattern=/^[a-z0-9\.]*@[a-z0-9\-]*\.[a-z]{2,5}$/
-console.log(pattern.test(myEmail))
+let password="jabeweb2021@!!!";
+
+pattern=/[a-zA-Z0-9@!#\.]*/
+console.log(pattern.test(password));
+
+
+// # Create a zipcode pattern
+
+let zipcode="3724";
+
+pattern=/[0-9]{4,8}/;
+console.log(pattern.test(zipcode));
 
 
 
