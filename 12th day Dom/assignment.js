4fd1345f7dbb1e2d90a1e8993b6ebf1d71fb6a96
jabed366 <jabedhasan231@gmail.com>
2021-12-24 21:09:58 +0600

    First Commit

---
+++ 12th day Dom/assignment.js
@@ -0,0 +1,77 @@
+const dev = [
+  {
+    devName: "Jabed",
+    skill: "Java",
+    location: "Mirpur",
+    salry: "200k",
+    devPhoto:
+      "https://images.squarespace-cdn.com/content/v1/5775c65d5016e1d65c1d815a/1525365771051-7YXRUSDH6P2TDP8PYPJ8/Passport-Photo-Man+%282%29.jpg",
+  },
+  {
+    devName: "Fahad",
+    skill: "Laravel",
+    location: "Mirpur",
+    salry: "200k",
+    devPhoto:
+      "https://images.squarespace-cdn.com/content/v1/54ff160de4b0a76e3a90696a/1562150296188-4XEX3JKWVO9CJX5I4UOO/us+American+passport+visa+photo+bath",
+  },
+  {
+    devName: "Nayeem",
+    skill: "Django",
+    location: "Mirpur",
+    salry: "200k",
+    devPhoto: "https://s3.ivisa.com/website-assets/blog/id-photo2.jpg",
+  },
+];
+
+////phoneBook////
+
+const phoneBook = [
+  {
+    numberSerial: 1,
+    name: "Jabed",
+    location: "Mirpur",
+    number: "01793534981",
+    photo: "https://s3.ivisa.com/website-assets/blog/id-photo2.jpg",
+  },
+  {
+    numberSerial: 2,
+    name: "Jabed",
+    location: "Mirpur",
+    number: "01793534981",
+    photo: "https://s3.ivisa.com/website-assets/blog/id-photo2.jpg",
+  },
+  {
+    numberSerial: 3,
+    name: "Jabed",
+    location: "Mirpur",
+    number: "01793534981",
+    photo: "https://s3.ivisa.com/website-assets/blog/id-photo2.jpg",
+  },
+];
+
+////student////
+
+const studentAll = [
+  {
+    numberSerial: 1,
+    name: "Jabed",
+    location: "Mirpur",
+    number: "01793534981",
+    photo: "https://s3.ivisa.com/website-assets/blog/id-photo2.jpg",
+  },
+  {
+    numberSerial: 2,
+    name: "Jabed",
+    location: "Mirpur",
+    number: "01793534981",
+    photo: "https://s3.ivisa.com/website-assets/blog/id-photo2.jpg",
+  },
+  {
+    numberSerial: 3,
+    name: "Jabed",
+    location: "Mirpur",
+    number: "01793534981",
+    photo: "https://s3.ivisa.com/website-assets/blog/id-photo2.jpg",
+  },
+];
--- 12th day Dom/db.js
+++ 12th day Dom/db.js
@@ -1,1 +1,56 @@
-console.log(document.title)
+const team = document.querySelector(".team");
+
+dev.map((data) => {
+  team.innerHTML += `
+
+<div class="team-memebrs">
+   <img src="${data.devPhoto}" alt="" />
+   <h3>${data.devName}</h3>
+   <h4>${data.skill}</h4>
+   <p>${data.location}</p>
+   <span>Salary : ${data.salry}<span>
+</div>
+`;
+});
+
+///phone Book
+
+const phoneBookList = document.querySelector(".faltu tbody");
+
+phoneBook.map((data) => {
+  phoneBookList.innerHTML += `
+<tr>
+  <td>${data.numberSerial}</td>
+  <td>${data.name}</td>
+  <td>${data.location}</td>
+  <td>${data.number}</td>
+  <td>
+    <img
+      src="${data.photo}"
+      alt=""
+    />
+  </td>
+</tr>
+`;
+});
+
+///student
+
+const studentList = document.querySelector(".studentSection tbody");
+
+studentAll.map((data) => {
+  studentList.innerHTML += `
+<tr>
+  <td>${data.numberSerial}</td>
+  <td>${data.name}</td>
+  <td>${data.location}</td>
+  <td>${data.number}</td>
+  <td>
+    <img
+      src="${data.photo}"
+      alt=""
+    />
+  </td>
+</tr>
+`;
+});
