07b3def4c5d1448f3a020a21f4bfbb9c3142ec0c
jabed366 <jabedhasan231@gmail.com>
2021-12-28 22:12:01 +0600

    First Commit

--- 13th day Dom/db.js
+++ 13th day Dom/db.js
@@ -1,5 +1,29 @@
-const jabed = document.querySelector("p");
+const button = document.querySelector(".button .button-click");
 
-jabed.innerHTML = "jabed";
-jabed.setAttribute("name", "khan");
-jabed.style.color = "red";
+// button.addEventListener("click", function () {
+//   alert();
+// });
+
+// button.addEventListener("dblclick", function () {
+//   alert();
+// });
+// button.addEventListener("mouseenter", function () {
+//   alert();
+// });
+// button.addEventListener("mouseleave", function () {
+//   alert();
+// });
+// button.addEventListener("mousedown", function () {
+//   alert();
+// });
+
+// button.addEventListener("mouseup", function () {
+//   alert();
+// });
+
+// button.addEventListener("click", () => {
+//   button.nextElementSibling.innerHTML = "We love Js";
+// });
+button.addEventListener("mouseover", () => {
+  button.nextElementSibling.innerHTML = "We love Js";
+});
