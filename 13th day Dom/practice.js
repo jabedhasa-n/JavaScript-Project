b7d7cafd5baf60d512b21be72ba7c573af565891
jabed366 <jabedhasan231@gmail.com>
2022-01-01 16:41:57 +0600

    First Commit

--- 13th day Dom/practice.js
+++ 13th day Dom/practice.js
@@ -10,3 +10,28 @@ querySelector()
 
 querySelectorAll()
 */
+
+// button.addEventListener("click", function () {
+//   alert();
+// });
+
+// button.addEventListener("dblclick", function () {
+//   alert();
+// });
+// button.addEventListener("mouseenter", function () {
+//   alert();
+// });
+// button.addEventListener("mouseleave", function () {
+//   alert();
+// });
+// button.addEventListener("mousedown", function () {
+//   alert();
+// });
+
+// button.addEventListener("mouseup", function () {
+//   alert();
+// });
+
+// button.addEventListener("click", () => {
+//   button.nextElementSibling.innerHTML = "We love Js";
+// });
