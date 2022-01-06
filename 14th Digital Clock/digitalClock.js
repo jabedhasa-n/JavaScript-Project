55aca6a8733f88e51d6c2dcbf26915adb71c66d7
jabed366 <jabedhasan231@gmail.com>
2022-01-06 21:22:23 +0600

    First Commit

--- 14th Digital Clock/digitalClock.js
+++ 14th Digital Clock/digitalClock.js
@@ -1,88 +1,88 @@
-const form = document.getElementById('form');
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const passwordtwo = document.getElementById('password2');
+const boxdate=document.querySelector(".boxdate h6")
+const boxmonth=document.querySelector(".boxmonth h6")
+const boxyear=document.querySelector(".boxyear h6")
 
 
-password.addEventListener('keyup',()=>{
-	const messages= password.parentElement;
-	const message=messages.querySelector('small')
-	let regularExpression  = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/;
-
-	if(regularExpression.test(password.value)==false){
-		message.innerText='username should be at least 6';
-
-	}
-	else if(password.value.length >16){
-		message.innerText='username should be more 16';
-	}
-	else{
-		message.innerText=' ';
-		message.classList.add("success");
-		message.style.border='1px solid red';
-	}
-})
-
-form.addEventListener('submit',function(e){
-	e.preventDefault();
-	checkValidation();
-});
 
 
-function checkValidation(){
-	const usernameValue=username.value;
-	const mailVlue=email.value;
-	const passwordValue=password.value;
-	const passwordtwoValue=passwordtwo.value;
-	if(usernameValue === '') {
-	valError(username, 'Username cannot be blank');
-	} 
-	else {
-		valsucc(username);     
-	};
-
-	if(mailVlue==''){
-		valError(email,'Email cann\'t be blank')
-	}
-	else if(!isEmail(mailVlue)){
-		valError(email,'Not a valid email')
-	}
-	else{
-		valsucc(email);
-	};
+setInterval(()=>{
+    let date=new Date();
+    let nowDate=date.getDate();
+    let nowMonth=date.getMonth()+1;
+    let nowYear=date.getFullYear();
+    boxdate.innerHTML=nowDate;
+    boxmonth.innerHTML=nowMonth;
+    boxyear.innerHTML=nowYear;
+},1000)
 
 
-	if(passwordValue==''){
-		valError(password,'password cann\'t be blank')
-	}
-	else{
-		valsucc(password);
-	};
 
-	if(passwordtwoValue==''){
-		valError(passwordtwo,'password cann\'t be blank')
-	}
-	else if(passwordValue!=passwordtwoValue){
-		valError(passwordtwo,'password does not match')
-	}
-	else{
-		valsucc(passwordtwo);
-	};
 
-}
-function valError(input, message) {
-	const formControl = input.parentElement;
-	const small = formControl.querySelector('small');
-	formControl.classList.add("error");
-	small.innerText = message;
-}
+const hour=document.querySelector(".boxhour h6")
+const minute=document.querySelector(".boxminute h6")
+const second=document.querySelector(".boxsecond h6")
 
-function valsucc(input) {
-	const formControl = input.parentElement;
-	formControl.classList.add("success");
-}
-	
-function isEmail(email) {
-	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
-}
+
+
+
+setInterval(()=>{
+    let date=new Date();
+    let nowHour=zero(date.getHours());
+    let nowMinute=zero(date.getMinutes());
+    let nowSceond=zero(date.getSeconds());
+    hour.innerHTML=nowHour<12 ? nowHour%12 : nowHour-12;
+    minute.innerHTML=nowMinute;
+    second.innerHTML=nowSceond;
+},1000)
+function zero(date){
+    if(date<10){
+        return `0${date}`
+    }
+    else{
+        return date;
+    }
+};
+
+//////
+
+
+// day
+
+const inputbutton=document.querySelector('input[type="radio"]:checked');
+const message=document.querySelector("form p");
+
+// inputbutton.addEventListener('click',()=>{
+//     message.innerHTML='oKKK';
+// })
+
+
+let dayToday=new Date();
+
+let day=dayToday.getDay();
+
+
+switch (day) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+       day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+  }
+
+message.innerHTML=day;
+
