ee795e53325526f5e0d60588bf484c885919c54e
jabed366 <jabedhasan231@gmail.com>
2022-01-06 22:47:29 +0600

    First Commit

--- 14th Digital Clock/digitalClock.js
+++ 14th Digital Clock/digitalClock.js
@@ -7,14 +7,13 @@ const boxyear=document.querySelector(".boxyear h6")
 
 setInterval(()=>{
     let date=new Date();
-    let nowDate=date.getDate();
-    let nowMonth=date.getMonth()+1;
+    let nowDate=zero(date.getDate());
+    let nowMonth=zero(date.getMonth()+1);
     let nowYear=date.getFullYear();
     boxdate.innerHTML=nowDate;
     boxmonth.innerHTML=nowMonth;
     boxyear.innerHTML=nowYear;
 },1000)
-
 
 
 
@@ -43,46 +42,48 @@ function zero(date){
     }
 };
 
-//////
 
+setInterval(()=>{
+  const inputbutton=document.querySelector('input[type="radio"]:checked');
+  const inputlabel1=document.querySelector('#sun');
+  const inputlabel2=document.querySelector('#mon');
+  const inputlabel3=document.querySelector('#tue');
+  const inputlabel4=document.querySelector('#wed');
+  const inputlabel5=document.querySelector('#thu');
+  const inputlabel6=document.querySelector('#fri');
+  const inputlabel7=document.querySelector('#sat');
+  const message=document.querySelector("form p");
+  
+  let dayToday=new Date();
+  let day=dayToday.getDay();
+  
+  switch (day) {
+      case 0:
+        day = "Sunday";
+        inputlabel1.style.color='red';
+        break;
+      case 1:
+        day = "Monday";
+        inputlabel2.style.color='red';
+        break;
+      case 2:
+         day = "Tuesday";
+         inputlabel3.style.color='red';
+        break;
+      case 3:
+        inputlabel4.style.color='red';
+        break;
+      case 4:
+        inputlabel5.style.color='red';
+        break;
+      case 5:
+        day = "Friday";
+        inputlabel6.style.color='red';
+        break;
+      case 6:
+        inputlabel5.style.color='red';
+        day = "Saturday";
+    }
+},1000)
 
-// day
-
-const inputbutton=document.querySelector('input[type="radio"]:checked');
-const message=document.querySelector("form p");
-
-// inputbutton.addEventListener('click',()=>{
-//     message.innerHTML='oKKK';
-// })
-
-
-let dayToday=new Date();
-
-let day=dayToday.getDay();
-
-
-switch (day) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-       day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-  }
-
-message.innerHTML=day;
 
