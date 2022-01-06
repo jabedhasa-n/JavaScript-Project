829fc227d199d574a52e4495dfc41a9d5ea11609
jabed366 <jabedhasan231@gmail.com>
2022-01-07 01:49:24 +0600

    First Commit

--- 14th Digital Clock/digitalClock.js
+++ 14th Digital Clock/digitalClock.js
@@ -1,8 +1,12 @@
+
+/**
+ * Date /month/year
+ */
+
+
 const boxdate=document.querySelector(".boxdate h6")
 const boxmonth=document.querySelector(".boxmonth h6")
 const boxyear=document.querySelector(".boxyear h6")
-
-
 
 
 setInterval(()=>{
@@ -15,7 +19,9 @@ setInterval(()=>{
     boxyear.innerHTML=nowYear;
 },1000)
 
-
+/**
+ * hour /minute/second
+ */
 
 const hour=document.querySelector(".boxhour h6")
 const minute=document.querySelector(".boxminute h6")
@@ -24,24 +30,18 @@ const second=document.querySelector(".boxsecond h6")
 
 setInterval(()=>{
     let date=new Date();
-    let nowHour=zero(date.getHours());
+    let nowHour=zero(date.getHours()%12 ||12);
     let nowMinute=zero(date.getMinutes());
     let nowSceond=zero(date.getSeconds());
-    hour.innerHTML=nowHour<12 ? nowHour%12 : nowHour-12;
+    hour.innerHTML=nowHour;
     minute.innerHTML=nowMinute;
     second.innerHTML=nowSceond;
 },1000)
 
-function zero(date){
-  if(date<10){
-      return `0${date}`
-  }
-  else{
-      return date;
-  }
-};
 
-
+/**
+ * all day
+ */
 const inputlabel1=document.querySelector('#sun');
 const inputlabel2=document.querySelector('#mon');
 const inputlabel3=document.querySelector('#tue');
@@ -55,40 +55,40 @@ setInterval(()=>{
   
   let dayToday=new Date();
   let day=dayToday.getDay();
-  
-  switch (day) {
-      case 0:
-        inputlabel1.style.color='red';
-        color(day);
-        break;
-      case 1:
-        inputlabel2.style.color='red';
-        color(day)
-        break;
-      case 2:
-         inputlabel3.style.color='red';
-         color(day)
-        break;
-      case 3:
-        inputlabel4.style.color='red';
-        color(day)
-        break;
-      case 4:
-        inputlabel5.style.color='red';
-        color(day)
-        break;
-      case 5:
-        inputlabel6.style.color='red';
-        color(day);
-        break;
-      case 6:
-        color(day)
-        inputlabel7.style.color='red';
-    }
+
+  colorText(day);
+  color(day);
 },1000)
 
 
+/**
+ * here is zeo function
+ * @param {*} date 
+ * @returns 
+ */
+
+function zero(date){
+  if(date<10){
+      return `0${date}`;
+  }
+  else{
+      return date;
+  }
+};
+
+/**
+ * her is the today Function
+ * @param {*} today 
+ */
 function color(today){
   const label=[inputlabel1,inputlabel2,inputlabel3,inputlabel4,inputlabel5,inputlabel6,inputlabel7];
   label[today].classList.add('active');
+}
+/**
+ * colorText today Function
+ * @param {*} today 
+ */
+function colorText(today){
+  const label=[inputlabel1,inputlabel2,inputlabel3,inputlabel4,inputlabel5,inputlabel6,inputlabel7];
+  label[today].style.color='red';
 }
