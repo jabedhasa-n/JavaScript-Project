ce8553e12f3a7c9e7790cff4c09e3ad144c0e1be
jabed366 <jabedhasan231@gmail.com>
2022-01-24 17:37:48 +0600

    First Commit

--- 15th Fiverr Order Counter/counter.js
+++ 15th Fiverr Order Counter/counter.js
@@ -1,44 +1,21 @@
-const start = document.getElementById('start')
-const stoped = document.getElementById('stoped')
-const reset = document.getElementById('reset')
-const count = document.getElementById('count')
-const progress = document.querySelector('.progress')
+const fiverOrder=document.getElementById('fiver_order');
 
-const h1=document.querySelector('h1');
+fiverOrder.addEventListener('submit',function (e){
+  e.preventDefault();
 
-let time;
-let counter;
-start.addEventListener('click',()=>{
-  counter=count.value
-  time=setInterval(() => {
-    h1.innerText=counter;
-    let width=progressFunction(count.value,counter);
-    progress.style.width=`${width}%`; 
+  
+  date=this.querySelector('input[type="date"]').value;
+  time=this.querySelector('input[type="time"]').value;
 
-    if(width>50 && width<=100){
-      progress.style.backgroundColor='red';
-    }
-    else if(width>30 && width<=50){
-      progress.style.backgroundColor='green';
-    }
-    else if(width>0 && width<=30){
-      progress.style.backgroundColor='orange';
-    }
-    if(counter==0){
-      clearInterval(time);
-    }
-    counter--;
-  }, 1000);
+
+  let start_time=new Date();
+  let end_time=new Date(date  + ' ' + time);
+
+  let time_diff=end_time.getTime()-start_time.getTime();
+  console.log(time_diff);
+
+  Math.floor(Math.abs(time_diff/10000));
+
+  
+
 })
-
-stoped.addEventListener('click',()=>{
-  clearInterval(time);
-})
-
-reset.addEventListener('click',()=>{
-  counter=0;
-  h1.innerText='Please Start Again ';
-  clearInterval(time);
-})
-
-
