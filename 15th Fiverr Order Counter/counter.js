3a5c5c53c58103ad055b41c444eafe93ae43e18b
jabed366 <jabedhasan231@gmail.com>
2022-01-24 19:24:31 +0600

    First Commit

--- 15th Fiverr Order Counter/counter.js
+++ 15th Fiverr Order Counter/counter.js
@@ -1,4 +1,5 @@
 const fiverOrder=document.getElementById('fiver_order');
+const counter=document.querySelector('.counter');
 
 fiverOrder.addEventListener('submit',function (e){
   e.preventDefault();
@@ -8,14 +9,25 @@ fiverOrder.addEventListener('submit',function (e){
   time=this.querySelector('input[type="time"]').value;
 
 
-  let start_time=new Date();
-  let end_time=new Date(date  + ' ' + time);
-
-  let time_diff=end_time.getTime()-start_time.getTime();
-  console.log(time_diff);
-
-  Math.floor(Math.abs(time_diff/10000));
-
+  setInterval(()=>{
+    let start_time=new Date();
+    let end_time=new Date(date  + ' ' + time);
   
+    let time_diff=Math.floor(Math.abs(end_time.getTime()-start_time.getTime()));
+  
+    let total_sec=Math.floor(time_diff/1000);
+    let total_min=Math.floor(total_sec/60);
+    let total_hours=Math.floor(total_min/60);
+    let total_day=Math.floor(total_hours/24);
+  
+  
+    let hours=total_hours-(total_day*24);
+    let min=total_min-(total_day*24*60)-(hours*60);
+    let sec=total_sec-(total_day*24*60*60)-(hours*60*60)-(min*60);
+    
+    // console.log(total_day  + ':' + hours + ':' + min + ':' +sec);
+    counter.innerHTML=`${total_day} : ${hours} : ${min} : ${sec}`;
+  
+  },1000)
 
 })
