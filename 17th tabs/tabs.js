b4afb785a8a7cca59c46c96132abd03aed2d32f6
jabed366 <jabedhasan231@gmail.com>
2022-02-06 00:47:00 +0600

    First Commit

--- 17th tabs/tabs.js
+++ 17th tabs/tabs.js
@@ -2,3 +2,20 @@
  * Get Elements
  */
 
+const tab_list=document.querySelectorAll('.tab-list-part ul li a');
+const tab_body=document.querySelectorAll('.tab_body');
+
+tab_list.forEach(data=>{
+    data.addEventListener('click',function(e){
+        e.preventDefault()
+        tab_list.forEach(data=>{
+            data.classList.remove('active')
+        })
+        data.classList.add('active')
+        let tab_bo=document.querySelector(data.getAttribute('href'));
+        tab_body.forEach(data=>{
+            data.classList.remove('active')
+        })
+        tab_bo.classList.add('active')
+    })
+})
